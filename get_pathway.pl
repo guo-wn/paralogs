3fdc3ed54e2793a61d37397cf5c2e6fa90427590
guo-wn <wennaguo123@163.com>
2017-07-11 10:16:22 +0800

    Create get_pathway.pl

---
+++ get_pathway.pl
@@ -0,0 +1,17 @@
+ 	#!/usr/bin/perl -w
+	use strict;
+	my @data;
+	my @data1;
+	my @data2;
+
+	open IN, "/home/wn/data/alignment/gene-paralog/Atha/pathway/Pathway.TXT" || die "$!";
+	while ( <IN> ){
+		chomp;
+		@data = split "\t",$_,7;
+		#print $data[1],"\n";
+		#@data1 = split ":",$data[1];
+		$data[5] =~ s/, /\n/g; 
+		open OUT,">$data[1].kegg.txt" or die $!;
+		print OUT $data[5],"\n";
+	}
+	close IN;
